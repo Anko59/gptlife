59439e5f4d5ff4347c2d4096544881f71bb617f6
Anko59 <mariedelahouce@gmx.fr>
2024-01-28 17:54:11 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="gptlife",
+    description="Life of LLMs, playing games",
+    version="1.0.0",
+    package_dir={"": "."},
+    packages=["gptlife"],
+    url="https://github.com/Anko59/GPTLife",
+    author="Anko59",
+    author_email="mariedelahouce@gmx.fr",
+    keywords=["mistral", "openai", "games", "scrapy", "real estate"],
+    include_package_data=True,
+)
